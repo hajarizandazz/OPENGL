17d9318b69ca22e3068985317301d3d69fbc0fda
LICHTHOMAS <zachary.savdie@tumo.world>
2024-06-11 19:16:07 +0200

    Added finger class, to be continued

---
+++ src/Finger.cpp
@@ -0,0 +1,24 @@
+//
+// Created by zacha on 11/06/2024.
+//
+
+#include "Finger.h"
+
+#include <glm/glm.hpp>
+#include "glm/ext.hpp"
+#include <glm/gtc/matrix_transform.hpp>
+
+#include <lighting_sphere.h>
+#include <shape.h>
+
+Finger::Finger(Shader *shader_program) : Shape(shader_program) {
+
+    glm::mat4 mat1 = glm::translate(glm::mat4(1.0f), glm::vec3(1.0f, 0.0f, -4.0f))
+        * glm::scale(glm::mat4(1.0f), glm::vec3(1.0f, 1.0f, 1.0f))
+        * glm::rotate(glm::mat4(1.0f), glm::radians(0.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+    Node* finger_node = new Node(mat1);
+
+    Shape* bone1 = new LightingSphere(shader_program,glm::vec3(0.0f, 1.0f, 0.0f),
+        glm::vec3(1.0f, 1.0f, 1.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+    finger_node->add(bone1);
+}
