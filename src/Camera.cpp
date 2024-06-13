c0ea8f1f3c3be03348cb910e8e0d39a33b997e40
Mimikkyuu <chloemathieu9@gmail.com>
2024-06-13 18:37:52 +0200

    caméra au sol qui ne vole plus

--- src/Camera.cpp
+++ src/Camera.cpp
@@ -38,6 +38,7 @@
             Position -= Right * velocity;
         if (direction == RIGHT)
             Position += Right * velocity;
+        Position.y=0.0f;
     }
 
     // processes input received from a mouse input system. Expects the offset value in both the x and y direction.
